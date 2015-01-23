395d2671fe5728abe75a3245587cf131837ef936
Daniel Abraján C <seorc.d@gmail.com>
2015-01-23 00:27:42 -0600

    Initial tree command implementation in ruby.

--- homework/tree.rb
+++ homework/tree.rb
@@ -3,4 +3,20 @@
 Build the unix tree command.
 It must show sub-trees.
 
+Usage:
+
+	$ ruby tree.rb [path]
+
 =end
+
+def tree_this(expr="", level=0)
+	Dir.glob("#{expr}#{expr.empty? ? '' : File::SEPARATOR}*") do |f|
+		puts "#{"\t" * level} #{File.directory?(f) ? '+' : '|'} #{f} [#{File.ftype f}]"
+		if File.directory? f
+			tree_this("#{f}", level + 1)
+		end
+	end
+end
+
+tree_this ARGV[0] || ""
+		
