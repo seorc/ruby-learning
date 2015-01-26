6f62f270f5f5128e75d68ca9f2073d373c1075ab
Daniel Abraján C <seorc.d@gmail.com>
2015-01-25 22:36:53 -0600

    Chatroom server ready :-)

--- homework/chatroom.rb
+++ homework/chatroom.rb
@@ -5,13 +5,39 @@ class ChatMaster
   def initialize
     @q = Queue.new
     @host = TCPServer.open(TCP_PORT)
-    @users = []
+    @users = {}
     start
     super
   end
 
-  def do_action(usr, action)
-    puts "Executed action #{aciton}"
+  def do_action(user, payload)
+    case payload[:action]
+    when "user"
+      register_as user, payload[:params]
+    when "bye"
+      kickof user
+    end
+  end
+
+  def register_as(user, name)
+    if name.nil?
+      user.socket.puts "You didn't specify a user name"
+      puts "Registering user #{user} #{name.nil?}"
+      return
+    end
+    if @users.keys.include? name
+      user.socket.puts "User '#{name}' has already been taken"
+    else
+      user.name = name
+      @users[name] = user
+      user.socket.puts "You're now known as '#{name}'"
+    end
+  end
+
+  def kickof(user)
+    user.socket.puts = "000EOF"
+    user.socket.close
+    @users.delete user.name
   end
 
   def start
@@ -20,29 +46,32 @@ class ChatMaster
         who, msg = @q.pop
         puts "A client sent: #{msg.class}"
         if msg.class == String
-          #action = /^\/(?<action>user|bye)( (?<params>\w*))?/.match(msg.strip)
-          #  action = /^\/(?<action>user|bye)/.match(msg.strip)
-          # if true || !action.nil?
-          #   do_action(who, "hoohoho") #action[:action])
-          # end
-            @users.each do |u|
-              if u.id != who
-                u.socket.puts "[#{who}] #{msg}"
-              end
+          payload = /^\/(?<action>user|bye)( +(?<params>\w*))?/.match(msg.strip)
+          if !payload.nil?
+             do_action(who, payload)
+             next
+          end
+          if who.name.nil?
+            who.socket.puts "You aren't registered yet, please do '/user yourname'"
+            next
+          end
+          @users.each do |name, u|
+            if u.id != who.id
+              u.socket.puts "[#{who.name}] #{msg}"
             end
-          #end
-        elsif msg.class == ChatUser
-          @users << msg
+          end
         end
 
       end
     end
+
     loop do
       Thread.new(@host.accept, @q) do |socket, q|
         usr = ChatUser.new(socket, q)
         
         socket.puts "Wellcome!\n"
-        socket.puts "You've been registered with ID #{usr.id}"
+        socket.puts "Please register using '/user yourname'"
+        socket.puts "To leave the chat, write '/bye'"
 
         q << [self, usr]
         usr.start
@@ -53,19 +82,20 @@ class ChatMaster
 end
 
 class ChatUser
-  attr_accessor :q, :master_q, :socket, :id
+  attr_accessor :q, :master_q, :socket, :id, :name
   @@ids = 0
   def initialize(socket, master_q)
-    @master_q = master_q
-    @socket = socket
     @@ids += 1
     @id = @@ids
+    @master_q = master_q
+    @socket = socket
+    @name = nil
   end
 
   def start
     loop do
       msg = @socket.gets
-      @master_q << [@id, msg]
+      @master_q << [self, msg]
     end
   end
 
